5caa53c1282b9e462117dedce946c5fdbda44717
rheadoshi <158753434+rheadoshi@users.noreply.github.com>
2025-01-02 10:52:57 +0530

    Fixed firebase configuration

--- src/service/firebaseConfig.jsx
+++ src/service/firebaseConfig.jsx
@@ -7,7 +7,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAYxMH2fGl5-LH57g7y6MGh78FagNryLCY",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "trip-crafter-v1.firebaseapp.com",
   projectId: "trip-crafter-v1",
   storageBucket: "trip-crafter-v1.firebasestorage.app",
